a0044608feb86c2391f51677e5bf5477bb86357f
Aloixius Divinagracia <aloixius.divinagracia@98labs.con>
2019-11-18 16:28:40 +0800

    Fixed merge conflict

--- scripts/generate-base.sh
+++ scripts/generate-base.sh
@@ -5,10 +5,7 @@ koi=$'\e[0;37;48;5;0mkoi\e[0m'
 error=$'\e[0;31;48;5;0mERR!\e[0m'
 success=$'\e[0;32;48;5;0mOK!\e[0m'
 info=$'\e[0;34;48;5;0mINFO\e[0m'
-<<<<<<< HEAD
-=======
 currentDir=$(dirname $(dirname $0))
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 
 abort() {
   echo >&2
@@ -22,11 +19,7 @@ abort() {
 trap 'abort' 0
 set -e
 echo $koi $info "Step 1/6: Generating Model:" $1".ts"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-models.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-models.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 trap : 0
 
@@ -34,11 +27,7 @@ trap 'abort' 0
 set -e
 echo ""
 echo $koi $info "Step 2/6: Generating Controller:" $1"Controller.ts"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-controllers.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-controllers.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 trap : 0
 
@@ -46,11 +35,7 @@ trap 'abort' 0
 set -e
 echo ""
 echo $koi $info "Step 3/6: Generating Service:" $1"Service.ts"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-services.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-services.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 trap : 0
 
@@ -58,11 +43,7 @@ trap 'abort' 0
 set -e
 echo ""
 echo $koi $info "Step 4/6: Generating Route:" $1".ts"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-routes.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-routes.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 trap : 0
 
@@ -70,11 +51,7 @@ trap 'abort' 0
 set -e
 echo ""
 echo $koi $info "Step 5/6: Generating Transformer:" $1"Transformer.ts"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-transformers.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-transformers.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 trap : 0
 
@@ -82,11 +59,7 @@ trap 'abort' 0
 set -e
 echo ""
 echo $koi $info "Step 6/6: Updating Index files"
-<<<<<<< HEAD
-node ./node_modules/koi-cli/src/generate-indexes.js $1
-=======
 $currentDir/lib/node_modules/koi-cli/src/generate-indexes.js $1
->>>>>>> 35bb6b9448d0339e3b379682c93fda5da420c692
 echo $koi $success " Success"
 echo ""
 trap : 0
